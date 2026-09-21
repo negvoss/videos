55231923cc2d2941d4867afd8839635e4c93ef15
Aaron Gostein <aaron.gostein@gmail.com>
2026-09-21 15:00:18 -0500

    Revised monster reveal animation and working on brute force strategy scene

--- _2026/turbo_the_snail/turbo.py
+++ _2026/turbo_the_snail/turbo.py
@@ -54,8 +54,10 @@ class Tile(Group):
 
 
 class Turbo(Sprite):
-    IDLE_KEYFRAME_0 = 0
-    IDLE_KEYFRAME_1 = 0.69
+    IDLE_KEYFRAME_START = 0
+    IDLE_KEYFRAME_END = 51 / 60
+    MOVE_KEYFRAME_START = IDLE_KEYFRAME_END
+    MOVE_KEYFRAME_END = 70 / 60
 
     def __init__(self, grid, *args, **kwargs):
         self.grid = grid
@@ -74,11 +76,15 @@ class Turbo(Sprite):
             OUT * 0.02
         )
 
-        self.time_tracker = ValueTracker(self.IDLE_KEYFRAME_0)
+        self.time_tracker = ValueTracker(self.IDLE_KEYFRAME_START)
         # Hacky fix below: if self.time_tracker.add_updater(lambda t: self.set_time(t.get_value())) is used,
         # it causes the tracker to get stuck oscillating between the two keyframe values, since the other .animates
         # create copies of the tracker.
         self.time_tracker.add_updater(lambda _: self.set_time(self.time_tracker.get_value()))
+
+        self.opacity_tracker = ValueTracker(1)
+        self.opacity_tracker.add_updater(lambda _: self.set_opacity(self.opacity_tracker.get_value()))
+        self.add(self.time_tracker, self.opacity_tracker)
 
     def move_to_position(self, i, j, **kwargs):
         target_tile = self.grid.get_tile(i, j)
@@ -93,11 +99,14 @@ class Turbo(Sprite):
 
         arc_axis = np.cross(move_vector, IN)
 
-        return self.move_to_position(
-            0, 0,
-            path_arc=PI * 0.8,
-            path_arc_axis=arc_axis,
-            **kwargs
+        return AnimationGroup(
+            self.move_to_position(
+                0, 0,
+                path_arc=PI * 0.8,
+                path_arc_axis=arc_axis,
+                **kwargs
+            ),
+            self.opacity_tracker.animate(run_time=0.4).set_value(1)
         )
 
     def move(self, direction, run_time=0.5):
@@ -116,15 +125,20 @@ class Turbo(Sprite):
         reveal_anim = self.grid.reveal_tile(*new_position, axis=[new_position[1] - j, new_position[0] - i, 0])
         if self.grid.is_monster(*new_position):
             reveal_anim = self.grid.reveal_monster(*new_position)
-        self.time_tracker.set_value(self.IDLE_KEYFRAME_0)
+        self.time_tracker.set_value(self.MOVE_KEYFRAME_START)
         return AnimationGroup(
             AnimationGroup(
                 self.move_to_position(*new_position),
-                self.time_tracker.animate.set_value(self.IDLE_KEYFRAME_1), run_time=run_time),
+                self.time_tracker.animate.set_value(self.MOVE_KEYFRAME_END), run_time=run_time),
             reveal_anim, lag_ratio=0.3)
 
 
 class Monster(Sprite):
+    REVEAL_KEYFRAME_START = 0
+    REVEAL_KEYFRAME_END = 57 / 60
+    BURROW_KEYFRAME_START = REVEAL_KEYFRAME_END
+    BURROW_KEYFRAME_END = 118 / 60
+
     def __init__(self, grid, i, j, *args, **kwargs):
         self.grid = grid
         super().__init__(
@@ -172,7 +186,8 @@ class TurboGrid(Group):
                 FadeIn(tile, shift=IN * 0.3)
                 for tile in tiles_sorted_from_center
             ],
-            FadeIn(self.turbo, shift=IN * 0.3),
+            self.turbo.shift(OUT * 0.3).animate.shift(IN * 0.3),
+            self.turbo.opacity_tracker.set_value(0).animate.set_value(1),
             FadeIn(self.monsters), lag_ratio=0.1)
 
     def get_tile(self, i, j):
@@ -216,7 +231,8 @@ class TurboGrid(Group):
         monster_row.remove(monster_tile)
         return AnimationGroup(
             monster_tile.reveal(),
-            monster.animate_set_time(1),
+            self.turbo.opacity_tracker.animate(run_time=0.6).set_value(0.1),
+            monster.animate_set_time(monster.REVEAL_KEYFRAME_END, run_time=run_time),
             AnimationGroup(
                 AnimationGroup(*[
                     t.reveal()
@@ -225,8 +241,17 @@ class TurboGrid(Group):
                 AnimationGroup(*[
                     t.reveal()
                     for t in monster_col
-                ], lag_ratio=0.1)
-            ), lag_ratio=0.2, run_time=run_time)
+                ], lag_ratio=0.1)), lag_ratio=0.2)
+
+
+def get_random_monster_positions(n):
+    monster_positions = []
+    remaining_columns = set(range(n - 1))
+    for j in range(1, n - 1):
+        i = random.choice(list(remaining_columns))
+        monster_positions.append((i, j))
+        remaining_columns.remove(i)
+    return monster_positions
 
 
 class TurboScene(InteractiveScene):
@@ -245,7 +270,7 @@ class TurboScene(InteractiveScene):
             self.play(
                 AnimationGroup(
                     self.turbo.move_to_start(),
-                    monster.animate_set_time(2), lag_ratio=0.6)
+                    monster.animate_set_time(monster.BURROW_KEYFRAME_END), lag_ratio=0.6)
             )
 
 
@@ -283,6 +308,41 @@ class TurboTest(TurboScene, ThreeDScene):
 
         # Move turbo
         moves = [RIGHT, RIGHT, RIGHT, DOWN, DOWN, DOWN]
+        for direction in moves:
+            self.move_turbo(direction)
+
+
+class BruteForce(TurboScene, ThreeDScene):
+    def __init__(self, *args, **kwargs):
+        n = 15
+        random.seed(3)
+        super().__init__(n, get_random_monster_positions(n), *args, **kwargs)
+
+    def construct(self):
+        # Set the camera
+        self.camera.frame.reorient(26, 58, 0, (-0.19, -0.72, -0.82), 8.21)
+
+        # Add the grid
+        grid, turbo = self.grid, self.turbo
+        self.play(
+            self.camera.frame.animate.reorient(-10, 28, 0, (-0.05, -1.26, -1.02), 15.91),
+            grid.create(), run_time=4)
+
+        # Show the initial positions of the monsters
+        shuffled_monsters = list(self.grid.monsters)
+        random.shuffle(shuffled_monsters)
+        self.play(
+            AnimationGroup(*[
+                monster.animate_set_time(monster.REVEAL_KEYFRAME_END)
+                for monster in shuffled_monsters
+            ], lag_ratio=0.1),
+            self.camera.frame.animate.reorient(0, 0, 0, (0, 0, 0), 16), run_time=2)
+        self.wait(1)
+
+        # Show how each column has at most one monster
+
+        # Move turbo
+        moves = [RIGHT] * 5 + [DOWN] * 14
         for direction in moves:
             self.move_turbo(direction)
         # moves = [DOWN, DOWN, DOWN, RIGHT, RIGHT, DOWN, RIGHT, DOWN, DOWN, DOWN, DOWN, DOWN]
